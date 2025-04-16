d451b6005872a72fe8f685d0bf192af61f011d1d
wlin23bu <02william.lin@gmail.com>
2025-04-16 18:29:31 -0400

    added intial databases

--- HW/HW3 caching/boxing/boxing/models/user_model.py
+++ HW/HW3 caching/boxing/boxing/models/user_model.py
@@ -14,7 +14,13 @@ configure_logger(logger)
 
 
 class Users():
+    __tablename__ = 'users'
 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    salt = db.Column(db.String(32), nullable=False)  # 16-byte salt in hex
+    password = db.Column(db.String(64), nullable=False)  # SHA-256 hash in hex
+    
     @staticmethod
     def _generate_hashed_password(password: str) -> tuple[str, str]:
         """
