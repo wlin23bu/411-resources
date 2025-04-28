8201f8ad5b3725aa5f60a20b1ddb8db238f63703
wlin23bu <02william.lin@gmail.com>
2025-04-28 16:14:28 -0400

    added tasks model and task_manager

--- README.md
+++
@@ -1,3 +0,0 @@
-# 411-resources
-
-Various resources helpful / necessary for BU's CS411 Software Engineering course
--- examples/ecomm_platform/warehousing/supplier_item.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class SupplierItem:
-    supplier_id: int
-    supplier_item_id: int
-    supplier_item_price: float
